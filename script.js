07edf190c62edca5a41c2222d10cee9f20799d0c
bimbim1214 <bimoadityapangestu@gmail.com>
2024-05-06 20:32:42 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function showAlert() {
+    alert("Yoo Brother Selamat datang, disini kamu bisa mendengarkan musik yang bisa untuk membangkitkan semangat kamu. TETAP SEMANGAT BROTHER!!!");
+}
+
+//pdw2024
